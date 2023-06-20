51a03dccfc2490d2081a7d212f9db4f801bb96eb
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-06-20 13:22:25 -0600

    add plane tri strip helper, gl program init

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,65 @@
 import { useRef, useEffect, FC } from 'react'
+import { initProgram, initBuffer } from '../lib/gl-wrap'
 
-const glacierSurfaceImg = '../../public/data/bedmap2_surface_rutford.png'
+const surfaceImg = '../../public/data/bedmap2_surface_rutford.png'
+
+const getPlaneVerts = (width: number, height: number): Float32Array => {
+    const verts = new Float32Array(999)
+    let vertInd = 0
+    const setStrip = (x: number, y: number, z: number): void => {
+        verts[vertInd++] = x
+        verts[vertInd++] = y
+        verts[vertInd++] = z
+
+        verts[vertInd++] = x + 1
+        verts[vertInd++] = y
+        verts[vertInd++] = z
+    }
+
+    for (let x = 0; x < width - 1; x++) {
+        // alternate y increment direction for each column
+        if (x % 2 === 0) {
+            for (let y = 0; y < height; y++) {
+                setStrip(x, y, 0)
+            }
+        } else {
+            for (let y = height - 1; y >= 0; y--) {
+                setStrip(x, y, 0)
+            }
+        }
+    }
+
+    return verts
+}
 
 const App: FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const glRef = useRef<WebGLRenderingContext | null>(null)
 
-    const initGl = (canvas: HTMLCanvasElement): void => {
+    const initGl = async (canvas: HTMLCanvasElement): Promise<void> => {
         const gl = canvas.getContext('webgl')
+        if (!gl) {
+            throw new Error('WebGL context creation failed')
+        }
+
+        // init program
+        const vertFile = '../shader/vert.glsl'
+        const fragFile = '../shader/frag.glsl'
+        const program = initProgram(gl, vertFile, fragFile)
+        gl.useProgram(program)
+
+        // init buffer
+        const plane = getPlaneVerts(10, 10)
+        const buffer = initBuffer(gl, plane, gl.STATIC_DRAW)
+
+        // init attribs
+
+        glRef.current = gl
     }
 
     useEffect(() => {
-        // get reference to gl context
         if (canvasRef.current) {
-            glRef.current = canvasRef.current.getContext('webgl')
+            initGl(canvasRef.current)
         }
     }, [])
 
