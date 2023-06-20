465db4fcac5d69035f21383c07da3e840f97af9a
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-06-20 14:13:58 -0600

    init basic gl drawing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,7 @@
-import { useRef, useEffect, FC } from 'react'
+import { useState, useRef, useEffect, FC } from 'react'
 import { mat4 } from 'gl-matrix'
 import { initProgram, initBuffer, initAttribute } from '../lib/gl-wrap'
+import styles from '../styles/app.module.css'
 
 const getPlaneVerts = (width: number, height: number): Float32Array => {
     const verts = new Float32Array((width - 1) * height * 6)
@@ -32,25 +33,28 @@ const getPlaneVerts = (width: number, height: number): Float32Array => {
 }
 
 const App: FC = () => {
+    const [gl, setGl] = useState<WebGLRenderingContext | null>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const glRef = useRef<WebGLRenderingContext | null>(null)
     const frameIdRef = useRef<number>(-1)
     const numVertRef = useRef<number>(-1)
 
     const initGl = async (canvas: HTMLCanvasElement): Promise<void> => {
+        canvas.width = window.innerWidth * window.devicePixelRatio
+        canvas.height = window.innerHeight * window.devicePixelRatio
         const gl = canvas.getContext('webgl')
         if (!gl) {
             throw new Error('WebGL context creation failed')
         }
 
         // init program
-        const vertFile = '../shader/vert.glsl'
-        const fragFile = '../shader/frag.glsl'
-        const program = initProgram(gl, vertFile, fragFile)
+        const vertFile = './shaders/vert.glsl'
+        const fragFile = './shaders/frag.glsl'
+        const program = await initProgram(gl, vertFile, fragFile)
 
         // init buffer
         const plane = getPlaneVerts(10, 10)
-        numVertRef.current = plane.length
+        numVertRef.current = plane.length / 3
         const buffer = initBuffer(gl, plane, gl.STATIC_DRAW)
 
         // init attribs
@@ -78,29 +82,30 @@ const App: FC = () => {
         gl.uniformMatrix4fv(uViewMatrix, false, viewMatrix)
         gl.uniformMatrix4fv(uProjMatrix, false, projMatrix)
 
-        glRef.current = gl
+        setGl(gl)
     }
 
     useEffect(() => {
         if (canvasRef.current) {
             initGl(canvasRef.current)
         }
+    }, [])
+
+    useEffect(() => {
+        if (!gl) { return }
         const draw = (): void => {
-            const gl = glRef.current
-            if (!gl) { return }
             gl.drawArrays(gl.TRIANGLE_STRIP, 0, numVertRef.current)
             frameIdRef.current = window.requestAnimationFrame(draw)
         }
         frameIdRef.current = window.requestAnimationFrame(draw)
-
         return () => {
             window.cancelAnimationFrame(frameIdRef.current)
         }
-    }, [])
+    }, [gl])
 
     return (
         <section>
-            <canvas ref={canvasRef} />
+            <canvas className={styles.canvas} ref={canvasRef} />
         </section>
     )
 }
