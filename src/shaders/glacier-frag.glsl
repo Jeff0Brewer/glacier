dd5e68edf791c4c4b666c6a43e1cc600b55e25ed
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-06-23 15:30:32 -0600

    add normal calc and shading to glacier

--- src/shaders/glacier-frag.glsl
+++ src/shaders/glacier-frag.glsl
@@ -1,7 +1,11 @@
 precision highp float;
 
-varying vec4 testColor;
+varying vec3 normal;
 
 void main() {
-    gl_FragColor = testColor;
+    vec3 color = vec3(0.9, 0.9, 1.0);
+    vec3 camera = vec3(0.0, 0.0, 1.0);
+    float shadeIntensity = 0.5;
+    float shade = dot(normal, camera) * shadeIntensity + (1.0 - shadeIntensity);
+    gl_FragColor = vec4(color * shade, 1.0);
 }
--- src/shaders/glacier-vert.glsl
+++ src/shaders/glacier-vert.glsl
@@ -4,14 +4,36 @@ attribute vec2 texCoord;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projMatrix;
-
 uniform sampler2D surfaceMap;
 
-varying vec4 testColor;
+varying vec3 normal;
+
+float heightMap(sampler2D map, vec2 coord) {
+    float scale = 0.1;
+    vec4 pixel = texture2D(map, coord);
+    float mag = length(pixel);
+    return mag * scale;
+}
+
+vec3 get3DPos(sampler2D map, vec2 coord) {
+    float height = heightMap(map, coord);
+    return vec3(coord, height);
+}
+
+vec3 getNormal(sampler2D map, vec2 coord) {
+    float delta = 0.005;
+    vec3 px = get3DPos(map, coord + vec2(delta, 0.0));
+    vec3 nx = get3DPos(map, coord + vec2(-delta, 0.0));
+    vec3 py = get3DPos(map, coord + vec2(0.0, delta));
+    vec3 ny = get3DPos(map, coord + vec2(0.0, -delta));
+    vec3 xVec = px - nx;
+    vec3 yVec = py - ny;
+    vec3 norm = cross(xVec, yVec);
+    return normalize(norm);
+}
 
 void main() {
-    float height = length(texture2D(surfaceMap, texCoord)) * .1;
+    float height = heightMap(surfaceMap, texCoord);
     gl_Position = projMatrix * viewMatrix * modelMatrix * vec4(position, height, 1.0);
-    float col = height*height * 10.0;
-    testColor = vec4(col, col, col, 1.0);
+    normal = getNormal(surfaceMap, texCoord);
 }
