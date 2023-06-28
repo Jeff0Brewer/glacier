fdaebeccd199e5ca4bcebb4b8bfe57d3fdff4b1d
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-06-28 10:05:16 -0600

    add async img load wrapper

--- src/lib/data-load.ts
+++ src/lib/data-load.ts
@@ -80,8 +80,23 @@ const loadDataset = async (): Promise<ModelData> => {
     return dataset
 }
 
+// wrap image load event in promise for async use
+const loadImageAsync = async (source: string): Promise<HTMLImageElement> => {
+    return new Promise((resolve, reject) => {
+        const image = new Image()
+        image.src = source
+        image.addEventListener('load', (): void => {
+            resolve(image)
+        })
+        image.addEventListener('error', (): void => {
+            reject(new Error(`Failed to load image ${source}`))
+        })
+    })
+}
+
 export {
     loadDataset,
+    loadImageAsync,
     WIDTH,
     HEIGHT
 }
