00e6c774bbf0672fe723f54f95aa5cc385818f08
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-02 11:22:29 -0600

    get img data on cpu for static surface verts

--- src/shaders/glacier-vert.glsl
+++ src/shaders/glacier-vert.glsl
@@ -1,41 +1,13 @@
-attribute vec2 position;
+attribute vec3 position;
 
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projMatrix;
 uniform mat4 scaleMatrix;
-uniform sampler2D surfaceMap;
-uniform vec2 dimensions;
-uniform float heightScale;
 
 varying vec3 normal;
 
-float heightMap(sampler2D map, vec2 texCoord, float scale) {
-    vec4 pixel = texture2D(map, texCoord);
-    float mag = length(pixel.xyz);
-    return mag * scale;
-}
-
-vec3 get3DPos(sampler2D map, vec2 pos, vec2 dims, float scale) {
-    vec2 texCoord = pos / dims;
-    float height = heightMap(map, texCoord, scale);
-    return vec3(pos, height);
-}
-
-vec3 getNormal(sampler2D map, vec2 pos, vec2 dims, float scale) {
-    float delta = 2.0;
-    vec3 px = get3DPos(map, pos + vec2(delta, 0.0), dims, scale);
-    vec3 nx = get3DPos(map, pos + vec2(-delta, 0.0), dims, scale);
-    vec3 py = get3DPos(map, pos + vec2(0.0, delta), dims, scale);
-    vec3 ny = get3DPos(map, pos + vec2(0.0, -delta), dims, scale);
-    vec3 xVec = px - nx;
-    vec3 yVec = py - ny;
-    vec3 norm = cross(xVec, yVec);
-    return normalize(norm);
-}
-
 void main() {
-    normal = getNormal(surfaceMap, position, dimensions, heightScale);
-    vec3 pos = get3DPos(surfaceMap, position, dimensions, heightScale);
-    gl_Position = projMatrix * viewMatrix * modelMatrix * scaleMatrix * vec4(pos, 1.0);
+    gl_Position = projMatrix * viewMatrix * modelMatrix * scaleMatrix * vec4(position, 1.0);
+    normal = vec3(0.0, 0.0, 1.0);
 }
--- src/vis/glacier.ts
+++ src/vis/glacier.ts
@@ -1,14 +1,13 @@
-import { mat4 } from 'gl-matrix'
-import { initProgram, initBuffer, initAttribute, initTexture } from '../lib/gl-wrap'
+import { mat4, vec3 } from 'gl-matrix'
+import { initProgram, initBuffer, initAttribute } from '../lib/gl-wrap'
 import vertSource from '../shaders/glacier-vert.glsl?raw'
 import fragSource from '../shaders/glacier-frag.glsl?raw'
 
 // floats per vertex for attribs
-const POS_FPV = 2
+const POS_FPV = 3
 
 class Glacier {
     buffer: WebGLBuffer
-    texture: WebGLTexture
     program: WebGLProgram
     bindPosition: () => void
     setModelMatrix: (mat: mat4) => void
@@ -28,19 +27,9 @@ class Glacier {
         this.program = initProgram(gl, vertSource, fragSource)
 
         this.buffer = initBuffer(gl)
-        const plane = getPlaneVerts(surface.width, surface.height)
-        gl.bufferData(gl.ARRAY_BUFFER, plane, gl.STATIC_DRAW)
-        this.numVertex = plane.length / POS_FPV
-
-        this.texture = initTexture(gl)
-        gl.texImage2D(
-            gl.TEXTURE_2D,
-            0,
-            gl.RGBA,
-            gl.RGBA,
-            gl.UNSIGNED_BYTE,
-            surface
-        )
+        const verts = getSurfaceVerts(surface, heightScale)
+        gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW)
+        this.numVertex = verts.length / POS_FPV
 
         this.bindPosition = initAttribute(gl, this.program, 'position', POS_FPV, POS_FPV, 0)
 
@@ -49,16 +38,12 @@ class Glacier {
         const uViewMatrix = gl.getUniformLocation(this.program, 'viewMatrix')
         const uProjMatrix = gl.getUniformLocation(this.program, 'projMatrix')
         const uScaleMatrix = gl.getUniformLocation(this.program, 'scaleMatrix')
-        const uHeightScale = gl.getUniformLocation(this.program, 'heightScale')
-        const uDimensions = gl.getUniformLocation(this.program, 'dimensions')
 
         // initialize uniforms
         gl.uniformMatrix4fv(uModelMatrix, false, model)
         gl.uniformMatrix4fv(uViewMatrix, false, view)
         gl.uniformMatrix4fv(uProjMatrix, false, proj)
         gl.uniformMatrix4fv(uScaleMatrix, false, scale)
-        gl.uniform1f(uHeightScale, heightScale)
-        gl.uniform2f(uDimensions, surface.width, surface.height)
 
         // get closures to easily set uniforms which may change
         this.setModelMatrix = (mat: mat4): void => { gl.uniformMatrix4fv(uModelMatrix, false, mat) }
@@ -69,7 +54,6 @@ class Glacier {
     draw (gl: WebGLRenderingContext, modelMatrix: mat4): void {
         gl.useProgram(this.program)
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer)
-        gl.bindTexture(gl.TEXTURE_2D, this.texture)
         this.bindPosition()
         this.setModelMatrix(modelMatrix)
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.numVertex)
@@ -77,23 +61,33 @@ class Glacier {
 }
 
 // from width and height, create plane triangle strip with position and tex coordinate attributes
-const getPlaneVerts = (width: number, height: number): Float32Array => {
+const getSurfaceVerts = (img: HTMLImageElement, heightScale: number): Float32Array => {
+    const imgReader = new ImageReader(img)
+
     // scale down dimensions, don't need a vertex at every image pixel
     const DOWNSAMPLE = 4
-    width = Math.ceil(width / DOWNSAMPLE)
-    height = Math.ceil(height / DOWNSAMPLE)
+    const width = Math.ceil(img.width / DOWNSAMPLE)
+    const height = Math.ceil(img.height / DOWNSAMPLE)
 
     const VERT_PER_POS = 2 // since drawing as triangle strip
     const verts = new Float32Array((width - 1) * height * VERT_PER_POS * POS_FPV)
 
     let ind = 0
-    // helper to set single line in plane's triangle strip
+
+    // helper to set vertex from x, y and height map
     // multiply by downsample to get coords aligned with pixel inds
+    const setVert = (x: number, y: number): void => {
+        const px = x * DOWNSAMPLE
+        const py = y * DOWNSAMPLE
+        verts[ind++] = px
+        verts[ind++] = py
+        verts[ind++] = vec3.length(imgReader.getRGB(px, py)) / 255 * heightScale
+    }
+
+    // helper to set single line in plane's triangle strip
     const setStrip = (x: number, y: number): void => {
-        verts[ind++] = x * DOWNSAMPLE
-        verts[ind++] = y * DOWNSAMPLE
-        verts[ind++] = (x + 1) * DOWNSAMPLE
-        verts[ind++] = y * DOWNSAMPLE
+        setVert(x, y)
+        setVert(x + 1, y)
     }
 
     for (let x = 0; x < width - 1; x++) {
@@ -110,6 +104,35 @@ const getPlaneVerts = (width: number, height: number): Float32Array => {
     }
 
     return verts
+}
+
+class ImageReader {
+    data: Uint8ClampedArray
+    width: number
+    height: number
+
+    constructor (img: HTMLImageElement) {
+        const canvas = document.createElement('canvas')
+        canvas.width = img.width
+        canvas.height = img.height
+
+        const ctx = canvas.getContext('2d')
+        if (!ctx) { throw new Error('Failed to get offscreen drawing context') }
+        ctx.drawImage(img, 0, 0)
+
+        this.data = ctx.getImageData(0, 0, img.width, img.height).data
+        this.width = img.width
+        this.height = img.height
+    }
+
+    getRGB (x: number, y: number): vec3 {
+        const ind = (y * this.width + x) * 4
+        return vec3.fromValues(
+            this.data[ind],
+            this.data[ind + 1],
+            this.data[ind + 2]
+        )
+    }
 }
 
 export default Glacier
