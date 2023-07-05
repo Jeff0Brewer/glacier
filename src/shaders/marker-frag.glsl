77f3618cc5b330457d0793772ebe19de21c0844b
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-05 12:17:50 -0600

    draw points as circles

--- src/shaders/marker-frag.glsl
+++ src/shaders/marker-frag.glsl
@@ -1,5 +1,10 @@
 precision highp float;
 
 void main() {
+    vec2 centerXY = 2.0 * gl_PointCoord - 1.0;
+    float radius = dot(centerXY, centerXY);
+    if (radius > 1.0) {
+        discard;
+    }
     gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
 }
