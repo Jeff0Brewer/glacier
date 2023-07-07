4260f39276bcdce87e6f23ea2e97e2f5f0cb40aa
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-07 13:10:09 -0600

    fix light variable name

--- src/shaders/glacier-frag.glsl
+++ src/shaders/glacier-frag.glsl
@@ -6,8 +6,8 @@ varying vec3 vNormal;
 varying vec2 vTexCoord;
 
 void main() {
-    vec3 camera = vec3(0.0, 0.0, 1.0);
-    float shade = dot(vNormal, camera);
+    vec3 light = vec3(0.0, 0.0, 1.0);
+    float shade = dot(vNormal, light);
     vec4 color = texture2D(texture, vTexCoord);
     gl_FragColor = vec4(color.xyz * shade, 1.0);
 }
