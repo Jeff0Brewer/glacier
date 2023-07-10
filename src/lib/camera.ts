9bd4c4346432d1e82502f24c0d36705eae8eda8c
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-10 17:10:58 -0600

    add pan functionality to camera

--- src/lib/camera.ts
+++ src/lib/camera.ts
@@ -1,17 +1,22 @@
 import { mat4, vec3 } from 'gl-matrix'
 
 const ROTATE_SPEED = 0.007
+const PAN_SPEED = 0.001
 const ZOOM_SPEED = 0.001
 
 class Camera {
     matrix: mat4 // matrix to apply camera transformations to
     axis: vec3 // axis for vertical mouse rotations
+    center: vec3 // position to zoom into, altered by panning
+    scale: number // track current scale for pan distance
     dragging: boolean
 
     constructor (matrix: mat4, eye: vec3, focus: vec3, up: vec3) {
         this.dragging = false
         this.matrix = matrix
         this.axis = vec3.create()
+        this.center = vec3.create()
+        this.scale = 1
         vec3.subtract(this.axis, focus, eye)
         vec3.cross(this.axis, this.axis, up)
         vec3.normalize(this.axis, this.axis)
@@ -22,7 +27,7 @@ class Camera {
         const dragFalse = (): void => { this.dragging = false }
         const mouseMove = (e: MouseEvent): void => {
             if (this.dragging) {
-                this.mouseRotate(e.movementX, e.movementY)
+                this.mousePan(e.movementX, e.movementY)
             }
         }
         const mouseWheel = (e: WheelEvent): void => {
@@ -58,9 +63,28 @@ class Camera {
         mat4.rotate(this.matrix, this.matrix, rotationX, this.axis)
     }
 
+    mousePan (dx: number, dy: number): void {
+        const axisY = vec3.cross(vec3.create(), this.axis, [0, 0, 1])
+        const speed = PAN_SPEED / this.scale
+
+        const translation = vec3.create()
+        vec3.scale(translation, this.axis, dx * speed)
+        vec3.scaleAndAdd(translation, translation, axisY, dy * speed)
+        mat4.translate(this.matrix, this.matrix, translation)
+
+        // translate center to track origin for zooming
+        vec3.add(this.center, this.center, translation)
+    }
+
     scrollZoom (delta: number): void {
         const scale = 1.0 + delta * ZOOM_SPEED
+
+        mat4.translate(this.matrix, this.matrix, vec3.scale(vec3.create(), this.center, -1))
         mat4.scale(this.matrix, this.matrix, [scale, scale, scale])
+        mat4.translate(this.matrix, this.matrix, this.center)
+
+        // update scale for uniform pan speed at any zoom
+        this.scale *= scale
     }
 }
 
