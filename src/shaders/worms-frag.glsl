974d4d0fa4b2eedef745b04c72d88e4d9a2dc2f7
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-10 23:50:29 -0600

    change worm color

--- src/shaders/worms-frag.glsl
+++ src/shaders/worms-frag.glsl
@@ -3,6 +3,6 @@ precision highp float;
 varying float fade;
 
 void main() {
-    vec3 color = mix(vec3(1.0, 1.0, 1.0), vec3(0.3, 0.3, 1.0), fade);
+    vec3 color = mix(vec3(1.0, 1.0, 1.0), vec3(0.76, 0.27, 0.6), fade);
     gl_FragColor = vec4(color, 1.0);
 }
