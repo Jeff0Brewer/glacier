d016a0a5cdd2875b15cccf2aaa5f7152947380e2
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-25 12:19:27 -0600

    add comment describing base color

--- src/shaders/marker-base-frag.glsl
+++ src/shaders/marker-base-frag.glsl
@@ -6,6 +6,7 @@ varying float vZ;
 void main() {
     vec3 light = vec3(0.0, 0.0, 1.0);
     float shade = clamp(dot(light, vNormal), 0.0, 1.0) * 0.5 + 0.5;
+    // use z position to create white / black sections on base
     float col = step(2.5, vZ);
     vec3 color = vec3(col, col, col);
     gl_FragColor = vec4(color * shade, 1.0);
